f3154596f2755c7265c5ebfd2205fc139cbcd546
nickmesh <59099009+nickmesh@users.noreply.github.com>
2020-01-30 15:28:20 -0800

    Lab 3.4 Complete

--- Meshcheryakov Personal Project/Assets/Scripts/PlayerController.cs
+++ Meshcheryakov Personal Project/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 8.0f;
     private Rigidbody playerRb;
-    
+    private float xBound = 15.0f;
+    private float zBound = 15.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +23,22 @@ public class PlayerController : MonoBehaviour
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+        if(transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+        }
     }
 }
