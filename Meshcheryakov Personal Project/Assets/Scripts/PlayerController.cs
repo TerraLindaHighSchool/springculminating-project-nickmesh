e7b12be41662f27e9088315e1106c0d45ec9e526
nickmesh <59099009+nickmesh@users.noreply.github.com>
2020-01-30 15:16:17 -0800

    Lab 3.3 Complete

--- Meshcheryakov Personal Project/Assets/Scripts/PlayerController.cs
+++ Meshcheryakov Personal Project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 8.0f;
+    private Rigidbody playerRb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
     }
 }
